d3255792a1d726c922bca7cc26de415107077949
Oladipupo kola <komolafe.solomon@yahoo.com>
2025-03-26 21:46:38 +0100

    Create Day_20.sh

---
+++ Day_20.sh
@@ -0,0 +1,12 @@
+🔹 Challenge 1: Create a DynamoDB table (Users) with user_id as the partition key + other attributes: name, email.
+🔹 Challenge 2: Insert 5+ records manually using AWS Console
+🔹 Challenge 3: Use AWS CLI to insert a record
+🔹 Challenge 4: Query a record using GetItem with the partition key
+
+🔹 Challenge 5: Update an existing item’s attribute using AWS CLI or SDK
+🔹 Challenge 6: Create a Global Secondary Index (GSI) on a different attribute (e.g., email)
+🔹 Challenge 7: Set up Time To Live (TTL) to auto-expire a record
+🔹 Challenge 8: Configure a CloudWatch Alarm for high read/write throughput
+🔹 Challenge 9: Use Python (Boto3) to script a CRUD operation workflow for the users table
+
+🔹 Challenge 10: Attach IAM permissions/role to allow read/write access via an EC2 and run challenge 9 codes from there
