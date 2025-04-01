232cdd0cf45c667494a42980fd4022af4e801acc
Oladipupo kola <komolafe.solomon@yahoo.com>
2025-04-01 23:13:29 +0200

    Create Day_24.sh

---
+++ Day_24.sh
@@ -0,0 +1,12 @@
+🔥 Challenges
+🔹 Challenge 1: Install Jenkins (locally or via Docker) and access the web UI
+🔹 Challenge 2: Create a Freestyle job that prints Hello from Jenkins
+🔹 Challenge 3: Add a GitHub repo to your job and clone it on build
+🔹 Challenge 4: Add a post-build step to send a notification to the console
+🔹 Challenge 5: Schedule your job to run every 5 minutes using cron syntax
+
+🔹 Challenge 6: Create a Freestyle job to run an Ansible playbook or Docker build
+🔹 Challenge 7: Install a plugin (e.g., Slack Notification or GitHub Integration)
+🔹 Challenge 8: Add credentials (SSH key or token) securely to Jenkins
+🔹 Challenge 9: Create a job that runs different shell scripts based on parameters
+🔹 Challenge 10: Configure Jenkins global tools: Java, Git, Maven, etc.
